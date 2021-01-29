b2d46dabad820ba3343e1c2fa7ceb7fe0d18b99b
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-29 12:21:24 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random()*100)+1
+
+let num3 = 44
+let num4 = 10
+
+let mod = num3 % num4
+
+let max = Math.max(1,2,3,4,5,20)
